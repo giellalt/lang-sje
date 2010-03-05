0064767454e8ae837bc4e62ef1860b5cd5dcd857
Trond Trosterud <trond.trosterud@uit.no>
2010-03-05 07:42:34 +0000

    perlscript to add new numeral.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sje@30735 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ src/smj2sje-num.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+use utf8 ;
+
+while (<>)
+
+{
+s/akta//g ;
+s/aktse//g ;
+s/duhát//g ;
+s/giehtja//g ;
+s/gietjav//g ;
+s/gietja//g ;
+s/guhtta//g ;
+s/guokta//g ;
+s/guok//g ;
+s/gáktsa//g ;
+s/gålmmå//g ;
+s/lågenan//g ;
+s/lågev//g ;
+s/låhke//g ;
+s/låhke#akta#tuvsán//g ;
+s/låk//g ;
+s/milijåvnnå//g ;
+s/miljona//g ;
+s/millijåvnnå//g ;
+s/milliárdda//g ;
+s/niellja//g ;
+s/niellje//g ;
+s/tjuode//g ;
+s/tjuohte//g ;
+s/tjuot//g ;
+s/tuvsán//g ;
+s/vihtta//g ;
+
+
+print ;
+}
