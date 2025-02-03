3a4b4535e6f854a4f43391d5cbaa56c651255f76
Trond Trosterud <trond.trosterud@uit.no>
2025-02-03 09:20:05 +0100

    hfst

--- devtools/noun_minip.sh
+++ devtools/noun_minip.sh
@@ -11,12 +11,13 @@
 
 
 LOOKUP=$(echo $LOOKUP)
+HLOOKUP=$(echo $HLOOKUP)
 GTLANGS=$(echo $GTLANGS)
 
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/stems/nouns.lexc | egrep $PATTERN |  tr '+' ':'|cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/nouns.lexc | egrep $PATTERN |  tr '+' ':'|cut -d ':' -f1>$L_FILE
 
 P_FILE="test/data/testnounparadigm.txt"
 
@@ -24,7 +25,8 @@ for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
-   echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-sje/src/generator-gt-norm.xfst
+#     echo "${lemma}${form}" | $LOOKUP $GTLANGS/lang-sje/src/fst/generator-gt-norm.xfst
+     echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sje/src/fst/generator-gt-norm.hfstol
  done
  rm -f $L_FILE
 done
